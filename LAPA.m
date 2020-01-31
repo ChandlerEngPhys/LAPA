8a32de8e59d95f7322501264f7048a6c5090c085
Chandler Aitken <chandleraitken@cmail.carleton.ca>
2020-01-30 22:38:51 -0500

    Completed PA:4

--- LAPA.m
+++ LAPA.m
@@ -1,19 +1,36 @@
 clear
-clf
+close all
 
 %Boundary conditions
-LBC = 1;
-RBC = 0;
 
-nx = 10;
-ny = 10;
-V = zeros(nx+1,ny+1);
-maxiterations = 5;
+nx = 20;
+ny = 20;
+V = zeros(nx,ny);
+maxiterations = 10;
 
-for iter = 1:1:maxiterations
-    for n=2:1:nx
-        for m=2:1:ny
+%Set boundary conditions 1
+for x = 1:nx
+    V(x,1) = 1;
+end
+%Set boundary conditions 2
+for x = 1:nx
+    V(x,ny)= 1;
+end
+
+for iter = 1:maxiterations
+    for n=2:1:nx-1
+        for m=2:1:ny-1
             V(m,n)=(V(m+1,n)+V(m-1,n)+V(m,n+1)+V(m,n-1))/4;
         end
     end
+    %Apply a filter
+%     filter = imboxfilt(V);
+%     V=filter;
+    %Plot variables
+    imagesc(V);
 end
+[ex,ey]=gradient(V);
+figure;
+surf(-ex,-ey);
+figure;
+quiver(-ex,-ey);
