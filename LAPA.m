cebf5bd94728c46a5bf21cad55589dedc81f06f1
Chandler Aitken <chandleraitken@cmail.carleton.ca>
2020-01-31 14:13:28 -0500

    Finished PA

--- LAPA.m
+++ LAPA.m
@@ -5,31 +5,40 @@ close all
 
 nx = 20;
 ny = 20;
-V = zeros(nx,ny);
-maxiterations = 10;
+V_new = zeros(nx,ny);
+V_old = zeros(nx,ny);
+maxiterations = 100;
 
 %Set boundary conditions 1
 for x = 1:nx
-    V(x,1) = 1;
+    V_new(x,1) = 1;
 end
 %Set boundary conditions 2
 for x = 1:nx
-    V(x,ny)= 1;
+    V_new(x,ny)= 1;
 end
 
 for iter = 1:maxiterations
     for n=2:1:nx-1
         for m=2:1:ny-1
-            V(m,n)=(V(m+1,n)+V(m-1,n)+V(m,n+1)+V(m,n-1))/4;
+            V_new(m,n)=(V_old(m+1,n)+V_old(m-1,n)+V_old(m,n+1)+V_old(m,n-1))/4;
         end
     end
+    %Free nodes
+%     for n=2:1:nx-1
+%         V_new(ny,n)=(V_old(m-1,n));
+%     end
+%     for n=2:1:nx-1
+%         V_new(1,n)=(V_old(m-1,n));
+%     end
+    V_old = V_new;
     %Apply a filter
-%     filter = imboxfilt(V);
-%     V=filter;
+%      filter = imboxfilt(V_new);
+%      V_new=filter;
     %Plot variables
-    imagesc(V);
+    surf(V_new);
 end
-[ex,ey]=gradient(V);
+[ex,ey]=gradient(V_new);
 figure;
 surf(-ex,-ey);
 figure;
